f0509eee1867ff78e842870520a420aad987050d
sammmedkamate <sammedkamate007@gmail.com>
2021-05-14 22:15:26 +0530

    third

--- cowin.py
+++ cowin.py
@@ -5,23 +5,6 @@ from pprint import pprint
 
 KEY = '1689920088:AAEWH9vem9qTxjxLxkGpNI5cyg0oKjPOTMg'
 
-# def slot():
-
-#     header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
-
-#     URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=590001&date=14-05-2021"
-#     response = requests.get(URL, headers=header).json()
-
-#     for hospital in response['centers']:
-#         print(hospital['name'])
-#         for day in hospital['sessions']:
-#             print(day['date'])
-#             if day['min_age_limit'] == 45 and day['available_capacity'] != 0:
-#                 print('seats: ', day['available_capacity'])
-#             else:
-#                 print('No slot')
-#             print('---------------------------')
-
 
 print('bot started')
 
@@ -32,6 +15,20 @@ def help_command(update, context):
     update.message.reply_text('Please contact Admin!')
 
 def handle_message(update, context):
+    header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
+
+    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=590001&date=14-05-2021"
+    response = requests.get(URL, headers=header).json()
+
+    for hospital in response['centers']:
+        print(hospital['name'])
+        for day in hospital['sessions']:
+            print(day['date'])
+            if day['min_age_limit'] == 45 and day['available_capacity'] != 0:
+                print('seats: ', day['available_capacity'])
+            else:
+                print('No slot')
+            print('---------------------------')
     update.message.reply_text('Hi, Im sammed kamate')
 
 def error(update, context):
